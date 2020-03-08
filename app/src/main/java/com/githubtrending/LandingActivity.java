aeb3a0cff0e00906853cd93814476f4f7b613320
Kevin Adhitama <vinzadhitama@yahoo.com>
2020-03-08 22:54:51 +0700

    initial commit

---
+++ app/src/main/java/com/githubtrending/LandingActivity.java
@@ -0,0 +1,14 @@
+package com.githubtrending;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_activity);
+    }
+}
