5261a1291391438c0c70694496ce8dd5a0b10e40
Kevin Adhitama <vinzadhitama@yahoo.com>
2020-03-09 10:29:33 +0700

    feat: add sort menu layout

--- app/src/main/java/com/githubtrending/LandingActivity.java
+++ app/src/main/java/com/githubtrending/LandingActivity.java
@@ -1,8 +1,13 @@
 package com.githubtrending;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_activity);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (R.id.sort_by_stars == item.getItemId()) {
+            //todo impl sort stars
+        } else if (R.id.sort_by_name == item.getItemId()) {
+            //todo impl sort name
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
