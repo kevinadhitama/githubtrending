6dbf7449cb8b69cc6c39b315316b195557cc67aa
Kevin Adhitama <vinzadhitama@yahoo.com>
2020-03-10 19:39:49 +0700

    feat: add error page widget

---
+++ app/src/main/java/com/githubtrending/page/landing/widget/ErrorStateWidget.java
@@ -0,0 +1,60 @@
+package com.githubtrending.page.landing.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+
+import com.githubtrending.R;
+import com.githubtrending.databinding.ErrorStateWidgetBinding;
+
+/**
+ * Created by kevin.adhitama on 10/03/20.
+ */
+public class ErrorStateWidget extends FrameLayout {
+
+    ErrorStateWidgetBinding mBinding;
+    Listener mListener;
+
+    public ErrorStateWidget(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        removeAllViews();
+
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.error_state_widget, this, true);
+
+        if (!isInEditMode()) {
+            mBinding = DataBindingUtil.bind(view);
+            initListener();
+            hide();
+        }
+    }
+
+    private void initListener() {
+        mBinding.buttonRetry.setOnClickListener(v -> {
+            if (mListener != null) {
+                mListener.onRetryClickListener();
+            }
+        });
+    }
+
+    public void setListener(Listener listener) {
+        mListener = listener;
+    }
+
+    public void show() {
+        setVisibility(VISIBLE);
+    }
+
+    public void hide() {
+        setVisibility(GONE);
+    }
+
+    interface Listener {
+        void onRetryClickListener();
+    }
+}
